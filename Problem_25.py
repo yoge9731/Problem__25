b6503a6ce686df436ac9001a18937cf8fef805df
YOGEESH K P <yogeesh793@gmail.com>
2025-01-15 17:30:44 +0530

    Add files via upload

---
+++ Problem_25.py
@@ -0,0 +1,8 @@
+"""Print Your Name Letter by Letter:
+
+Write a program that takes your name as input and prints each letter of your name using a for loop."""
+
+Name = input("Enter your name: ")
+for i in Name:
+    print(i)
+    
